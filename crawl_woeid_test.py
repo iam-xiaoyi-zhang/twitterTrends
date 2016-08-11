bc4b1ed4b7816a43e39ab376f1a72b4b60450bfe
Xiaoyi Zhang <iamxyz@bu.edu>
2016-08-11 18:06:47 -0400

    from last crawl_woeid.py
    put crawl_woeid.py into it and did a little change in order of code

---
+++ crawl_woeid_test.py
@@ -0,0 +1,53 @@
+import os
+import urllib, urllib2
+from bs4 import BeautifulSoup
+import pymongo
+
+# set root url
+rooturl = 'http://woeid.rosselliot.co.nz/lookup/'
+
+# set connection to mongodb
+MONGODB_SERVER = "1-PC" # does "localhost" work?
+MONGODB_PORT = 27017
+MONGODB_DB = "test"
+MONGODB_COLLECTION_WOEID = "newwoeid"
+
+print("-----CONNECTING TO MONGODB-----")
+connection = pymongo.MongoClient(
+    MONGODB_SERVER,
+    MONGODB_PORT
+)
+db = connection[MONGODB_DB]
+collection = db[MONGODB_COLLECTION_WOEID]
+
+
+
+# set url
+uri = 'United States'
+url = rooturl + uri
+
+# send request and get response from website
+print("-----SENDING REQUEST-----")
+response = urllib2.urlopen(url)
+html = response.read()
+
+# get woeid and other info from html doc
+soup = BeautifulSoup(html, 'html.parser')
+infoList = []
+for line in soup.find_all('tr')[1:]:
+    info = {"city": line['data-city'],
+            "district_county": line['data-district_county'],
+            "province_state": line['data-province_state'],
+            "country": line['data-country'],
+            "woeid": line['data-woeid']}
+    infoList.append(info)
+
+# check data in pymongo and then insert new data
+for info in infoList:
+    # check if info with the woeid exists
+    if not collection.find_one({"woeid": info['woeid']}):
+        # insert
+        collection.insert(info)
+        print("info added to MongoDB database!")
+
+os.system('pause')
