7f4e9c851a13afb6121351151c3328cf5394ea2b
Xiaoyi Zhang <iamxyz@bu.edu>
2016-09-07 14:42:58 -0400

    Added API keys

---
+++ api.py
@@ -0,0 +1,9 @@
+# Twitter API
+# consumer key
+twitter_consumer_key = "rivSg5C3w0kkXdYpJmfhyqZOV"
+# consumer secret
+twitter_consumer_secret = "ZYt4NVyoKPvlvr7nXbqh41bjiUk5MwrxkkXO6zsWPTwpe4tFlZ"
+# access token
+twitter_access_token = "740893267563282433-GTsXSnMZyjZ0PwtaCI3Bh4xfAz4CPYW"
+# access secret
+twitter_access_secret = "XiTBRpORylQxr3Zwp3HNwaadYodksQXyBtsArRolAAGD7"
